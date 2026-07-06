2f3117691710694ea6e9f26df11ff58c72f5a590
troilus <iswangli@outlook.com>
2026-07-06 10:58:48 +0800

    菜单显示优化

--- App/ui/menu.c
+++ App/ui/menu.c
@@ -621,18 +621,7 @@ void UI_DisplayMenu(void)
 
     UI_DisplayClear();
 
-#ifdef ENABLE_FEAT_F4HWN
-    UI_DrawLineBuffer(gFrameBuffer, 48, 0, 48, 55, 1); // Be ware, status zone = 8 lines, the rest = 56 ->total 64
-    //UI_DrawLineDottedBuffer(gFrameBuffer, 0, 46, 50, 46, 1);
-
-    for (uint8_t i = 0; i < 48; i += 2)
-    {
-        gFrameBuffer[5][i] = 0x40;
-    }
-#endif
-
 #ifndef ENABLE_CUSTOM_MENU_LAYOUT
-        // original menu layout
     for (i = 0; i < 3; i++)
         if (gMenuCursor > 0 || i > 0)
             if ((gMenuListCount - 1) != gMenuCursor || i != 2)
